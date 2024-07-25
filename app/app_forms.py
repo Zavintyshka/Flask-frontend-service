7148588b77fca136d3c61e5c0d6abeb8b31f1d7a
Vadim Sosnin <VadimSosnin02@yandex.ru>
2024-07-25 13:47:45 +0300

    Added validators for user profile form

--- app/app_forms.py
+++ app/app_forms.py
@@ -1,9 +1,23 @@
-from wtforms import Form, StringField, EmailField
+from wtforms import Form, StringField, EmailField, validators
 
 
 class UserDataForm(Form):
-    firstname = StringField("Firstname:")
-    lastname = StringField("Lastname:")
+    firstname = StringField("Firstname:",
+                            validators=[
+                                validators.Length(min=4, max=20,
+                                                  message="Firstname must be between 4 and 20 characters"),
+                                validators.Regexp("^[A-Za-z]+$",
+                                                  message="Firstname must contain only a-z, A-Z characters")
+                            ]
+                            )
+    lastname = StringField("Lastname:",
+                           validators=[
+                               validators.Length(min=4, max=20,
+                                                 message="Lastname must be between 4 and 20 characters"),
+                               validators.Regexp("^[A-Za-z]+$",
+                                                 message="Lastname must contain only a-z, A-Z characters")
+                           ]
+                           )
     username = StringField("Username:", render_kw={"readonly": True, "style": "color:gray"})
     email = EmailField("E-mail:")
     registered_at = StringField("Registered:", render_kw={"readonly": True, "style": "color:gray"})
--- app/user/views.py
+++ app/user/views.py
@@ -7,26 +7,31 @@ from datetime import datetime
 user_blueprint = Blueprint("user", __name__)
 
 
-# Registration
-
 @user_blueprint.route("/user-profile/", methods=["GET", "POST"])
 def user_profile_view():
     user_data = g.user["user"]
+
     if not user_data:
         return redirect(url_for("user.not_authorized_view"))
 
-    user_data["registered_at"] = datetime.strptime(user_data["registered_at"], "%Y-%m-%dT%H:%M:%S.%f%z").strftime(
-        "%m/%d/%Y %H:%M")
+    user_data["registered_at"] = beautify_date(user_data["registered_at"])
     filled_form = UserDataForm(**user_data)
     match request.method:
         case "GET":
             return render_template("user/user-profile.html", form=filled_form, user=user_data)
         case "POST":
-            new_user_data_form = dict(request.form)
+            user_data_form = UserDataForm(request.form)
+
+            if not user_data_form.validate():
+                return render_template("user/user-profile.html", form=user_data_form, user=user_data)
+
+            user_data_json = user_data_form.data
+            del user_data_json["registered_at"]
+            del user_data_json["username"]
             url = f"{settings.API_GATEWAY_URL}/user/account/"
             jwt_token = request.cookies["jwt_token"]
             header = {"Authorization": f"Bearer {jwt_token}"}
-            requests.put(url, headers=header, json=new_user_data_form)
+            requests.put(url, headers=header, json=user_data_json)
             return redirect(url_for("user.user_profile_view"))
 
 
@@ -46,6 +51,7 @@ def incorrect_registration_data_view():
     return render_template("errors/standard_error_page.html",
                            message="Some of your data was incorrect while registration process. Please try again.")
 
+
 @user_blueprint.get("/user-profile/not-authorized/")
 def not_authorized_view():
     return render_template("errors/standard_error_page.html",
@@ -57,3 +63,7 @@ def logout_view():
     response = make_response(redirect(location=url_for("index.index_get")))
     response.set_cookie("jwt_token", '', expires=0, httponly=True)
     return response
+
+
+def beautify_date(iso8086: str) -> str:
+    return datetime.strptime(iso8086, "%Y-%m-%dT%H:%M:%S.%f%z").strftime("%m/%d/%Y %H:%M")
