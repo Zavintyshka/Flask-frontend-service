81d40102898a3799a45a880f49cccd0994b4a144
Vadim Sosnin <VadimSosnin02@yandex.ru>
2024-08-07 15:18:17 +0300

    Improved File Naming and Download Functionality - Converted file names now retain the original name with the addition of a "_converted" suffix - Downloaded files now have consistent and appropriate naming conventions

--- app/static/js/user_files.js
+++ app/static/js/user_files.js
@@ -1,36 +1,39 @@
 const download_buttons = document.querySelectorAll(".download_button")
+
 const jwt_token = document.cookie.split("jwt_token=")[1]
+
+
+const download_file = (url, downloading_filename) => {
+    fetch(url, {
+        method: "GET",
+        headers: {
+            "Authorization": `Bearer ${jwt_token}`
+        }
+    }).then(response => {
+        if (!response.ok) {
+            alert(`Something went wrong. Status: ${response.status}`)
+        }
+        return response.blob()
+    }).then(data => {
+            const url = URL.createObjectURL(data);
+            const anchor = document.createElement("a");
+            anchor.href = url;
+            anchor.download = downloading_filename;
+            anchor.style.display = "none";
+            document.body.append(anchor)
+            anchor.click();
+            anchor.remove();
+        }
+    )
+}
+
 
 for (let i = 0; i < download_buttons.length; i++) {
     const download_button = download_buttons[i];
+    const parent = download_button.parentElement.parentElement;
+    const filename = parent.querySelector(".part_filename").querySelector("h4").textContent
     const download_link = download_button.getAttribute("download_link");
-
     download_button.addEventListener("click", () => {
-        fetch(download_link, {
-            method: "GET",
-            headers: {
-                "Authorization": `Bearer ${jwt_token}`
-            }
-        })
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error("Failed to fetch the file. Status: " + response.status);
-                }
-                return response.blob();
-            })
-            .then(blob => {
-                const url = URL.createObjectURL(blob);
-                const a = document.createElement('a');
-                a.style.display = 'none';
-                a.href = url;
-                a.download = 'downloaded-file';
-                document.body.appendChild(a);
-                a.click();
-                URL.revokeObjectURL(url);
-            })
-            .catch(error => {
-                console.error("Error:", error);
-                alert(error);
-            });
-    });
+        download_file(download_link, filename)
+    })
 }
